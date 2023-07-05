f0af6cad172854af35b668a16417b23ade00f0e8
Youssab10 <117522374+Youssab10@users.noreply.github.com>
2023-07-06 02:17:37 +0300

    second upload

--- lib/modules/screens/map/view.dart
+++ lib/modules/screens/map/view.dart
@@ -1,16 +1,14 @@
 import 'dart:async';
+
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import '../../../layout/cubit/app_cubit.dart';
-import 'model.dart';
-import 'model1.dart';
+import 'package:twsela/modules/screens/map/model.dart';
+import 'package:twsela/modules/screens/map/model1.dart';
 
 class MyMap extends StatefulWidget {
-  MyMap({
-    Key? key,
-  }) : super(key: key);
+  const MyMap({Key? key}) : super(key: key);
 
   @override
   State<MyMap> createState() => _MyMapState();
@@ -20,11 +18,18 @@ class _MyMapState extends State<MyMap> {
   late Position cl;
   late double x, y;
   late StreamSubscription<Position> positionStream;
+
   Future getPostion() async {
     bool services;
     LocationPermission per;
     services = await Geolocator.isLocationServiceEnabled();
-    if (services == false) {}
+    if (services == false) {
+      // AwesomeDialog(
+      //     context: context,
+      //     title: "services",
+      //     body: Text("Services is not Enabled"))
+      //   ..show();
+    }
     per = await Geolocator.checkPermission();
     if (per == LocationPermission.denied) {
       per = await Geolocator.requestPermission();
@@ -46,7 +51,7 @@ class _MyMapState extends State<MyMap> {
     super.initState();
     getPostion();
     polylines;
-
+    s();
     positionStream =
         Geolocator.getPositionStream().listen((Position? position) {
       changeMarker(position!.latitude, position.longitude);
@@ -68,77 +73,74 @@ class _MyMapState extends State<MyMap> {
         icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
         markerId: MarkerId("Location"),
         position: LatLng(lat, long)));
-
+    // goTo(lat, long);
     setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    AppCubit cubit = AppCubit.get(context);
     return Scaffold(
-      body: Column(
-        children: [
-          TextFormField(
-            controller: cubit.fromController,
-            decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'From',
-                prefixIcon: Icon(
-                  Icons.location_pin,
-                ),
-                filled: true,
-                fillColor: Colors.white,
-                hintText: "Search"),
-            onFieldSubmitted: (value) async {
-              final resp = await Dio().get(
-                  "https://api.openrouteservice.org/geocode/search?api_key=5b3ce3597851110001cf6248f3dacb8768e34a17a5f94cf6d01a1277&text=$value&boundary.country=EG&size=360");
-              final model = SearchData.fromJson(resp.data);
-              showMenu(
-                context: context,
-                position: const RelativeRect.fromLTRB(16, 195, 16, 0),
-                constraints: BoxConstraints(
-                  minWidth: MediaQuery.of(context).size.width - 25,
-                  maxWidth: MediaQuery.of(context).size.width - 25,
-                  maxHeight: 400,
-                  minHeight: 200,
-                ),
-                items: List.generate(
-                  model.results.length,
-                  (index) => PopupMenuItem(
-                    child: Text(model.results[index].name),
-                    onTap: () {
-                      setState(() {});
-                    },
+      body: Stack(children: [
+        GoogleMap(
+          polylines: polylines,
+          markers: markers,
+          onTap: (argument) {
+            markers.add(Marker(
+                markerId: MarkerId(argument.longitude.toString()),
+                position: LatLng(argument.latitude.toDouble(),
+                    argument.longitude.toDouble())));
+            setState(() {});
+          },
+          mapType: MapType.normal,
+          initialCameraPosition: cairo,
+          onMapCreated: (GoogleMapController controller) {
+            completer.complete(controller);
+          },
+        ),
+        SafeArea(
+          child: Padding(
+            padding: const EdgeInsetsDirectional.symmetric(
+                horizontal: 10, vertical: 10),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                  filled: true, fillColor: Colors.white, hintText: "Search"),
+              onFieldSubmitted: (value) async {
+                final resp = await Dio().get(
+                    "https://api.openrouteservice.org/geocode/search?api_key=5b3ce3597851110001cf6248f3dacb8768e34a17a5f94cf6d01a1277&text=$value&boundary.country=EG&size=360");
+                final model = SearchData.fromJson(resp.data);
+                showMenu(
+                  context: context,
+                  position: const RelativeRect.fromLTRB(16, 88, 16, 0),
+                  constraints: BoxConstraints(
+                    minWidth: MediaQuery.of(context).size.width - 18,
+                    maxWidth: MediaQuery.of(context).size.width - 18,
+                    maxHeight: 400,
+                    minHeight: 200,
                   ),
-                ),
-              );
-            },
-            validator: (value) {
-              if (value!.isEmpty) {
-                return 'Enter Location';
-              } else {
-                return null;
-              }
-            },
+                  items: List.generate(
+                    model.results.length,
+                    (index) => PopupMenuItem(
+                      child: Text(model.results[index].name),
+                      onTap: () {
+                        markers.add(Marker(
+                            markerId: MarkerId("1"),
+                            position: LatLng(
+                                model.results[index].lat.toDouble(),
+                                model.results[index].lng.toDouble())));
+                        goTo(model.results[index].lat.toDouble(),
+                            model.results[index].lng.toDouble());
+                        setState(() {
+                          s();
+                        });
+                      },
+                    ),
+                  ),
+                );
+              },
+            ),
           ),
-          GoogleMap(
-            polylines: polylines,
-            markers: markers,
-            onTap: (argument) {
-              markers.add(Marker(
-                  markerId: MarkerId(argument.longitude.toString()),
-                  position: LatLng(argument.latitude.toDouble(),
-                      argument.longitude.toDouble())));
-              setState(() {});
-            },
-            mapType: MapType.normal,
-            initialCameraPosition: cairo,
-            onMapCreated: (GoogleMapController controller) {
-              completer.complete(controller);
-            },
-          ),
-        ],
-      ),
+        ),
+      ]),
     );
   }
 
@@ -148,18 +150,15 @@ class _MyMapState extends State<MyMap> {
         CameraPosition(target: LatLng(lat, lng), zoom: 16)));
   }
 
-  late LatLng previousPosition;
-
-  s(num fromLong, num fromLat, num toLong, num toLat) async {
+  s() async {
     final dis = await Dio().get(
-        "https://api.openrouteservice.org/v2/directions/driving-car?api_key=5b3ce3597851110001cf6248f3dacb8768e34a17a5f94cf6d01a1277&start=${fromLong},${fromLat}&end=${toLong},${toLat}");
-
+        "https://api.openrouteservice.org/v2/directions/driving-car?api_key=5b3ce3597851110001cf6248f3dacb8768e34a17a5f94cf6d01a1277&start=${y},${x}&end=${markers.last.position.longitude},${markers.last.position.latitude}");
+    //${markers.position.latitude},${markers.last.position.longitude}
     final model1 = DrawLineData.fromJson(dis.data);
-    polylines.clear();
     polylines.add(Polyline(
         polylineId: PolylineId("ds"),
         color: Colors.blueAccent,
-        width: 10,
+        width: 6,
         points: model1.listOfPoints));
   }
 }
